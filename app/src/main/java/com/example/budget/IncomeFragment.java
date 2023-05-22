1ab53b6bbf26beb1093ad315f0cb5e6c272a6543
Eric Cheyne <echeyne@csu.fullerton.edu>
2023-05-22 04:07:46 -0700

    Fixed some bugs in the IncomeFragment file.

--- app/src/main/java/com/example/budget/IncomeFragment.java
+++ app/src/main/java/com/example/budget/IncomeFragment.java
@@ -1,64 +1,124 @@
 package com.example.budget;
 
+import static com.example.budget.R.layout.income_recycler_data;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link IncomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.budget.Model.Data;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerAdapter_LifecycleAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
 public class IncomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //Firebase Database
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mIncomeDatabase;
 
-    public IncomeFragment() {
-        // Required empty public constructor
-    }
+    //Recycler View
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment IncomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static IncomeFragment newInstance(String param1, String param2) {
-        IncomeFragment fragment = new IncomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private RecyclerView recyclerView;
+    private FirebaseRecyclerAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_income, container, false);
+        View myview = inflater.inflate(R.layout.fragment_income, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uid = mUser.getUid();
+
+        mIncomeDatabase = FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
+
+        recyclerView = myview.findViewById(R.id.recycler_id_income);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        return myview;
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Data> options = new FirebaseRecyclerOptions.Builder<Data>()
+                .setQuery(mIncomeDatabase, Data.class)
+                .build();
+
+        adapter = new FirebaseRecyclerAdapter<Data, MyViewHolder>(options) {
+
+            public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.income_recycler_data, parent, false));
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull Data model) {
+                holder.setAmmount(model.getAmount());
+                holder.setType(model.getType());
+                holder.setNote(model.getNote());
+                holder.setDate(model.getDate());
+            }
+
+        };
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        private void setType(String type) {
+            TextView mType = mView.findViewById(R.id.type_txt_income);
+            mType.setText(type);
+        }
+
+        private void setNote(String note) {
+
+            TextView mNote = mView.findViewById(R.id.note_txt_income);
+            mNote.setText(note);
+        }
+
+        private void setDate(String date) {
+            TextView mDate = mView.findViewById(R.id.date_txt_income);
+            mDate.setText(date);
+        }
+
+        private void setAmmount(int ammount) {
+            TextView mAmmount = mView.findViewById(R.id.ammount_txt_income);
+            String stammount = String.valueOf(ammount);
+            mAmmount.setText(stammount);
+        }
     }
 }
+
